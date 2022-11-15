d21626729199a695034ac82c37eafe043bf63a05
Sunbin Kim <einsbon@gmail.com>
2022-11-16 06:11:30 +0900

    Add project files.

---
+++ InertiaEstimator/Form1.Designer.cs
@@ -0,0 +1,235 @@
+﻿namespace InertialEstimator
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonStart = new System.Windows.Forms.Button();
+            this.buttonChooseFile = new System.Windows.Forms.Button();
+            this.labelFilePath = new System.Windows.Forms.Label();
+            this.textBoxOriginX = new System.Windows.Forms.TextBox();
+            this.textBoxOriginY = new System.Windows.Forms.TextBox();
+            this.textBoxOriginZ = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.textBoxResult = new System.Windows.Forms.TextBox();
+            this.textBoxDensity = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBoxDiameter = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // buttonStart
+            // 
+            this.buttonStart.Location = new System.Drawing.Point(30, 236);
+            this.buttonStart.Name = "buttonStart";
+            this.buttonStart.Size = new System.Drawing.Size(112, 34);
+            this.buttonStart.TabIndex = 5;
+            this.buttonStart.Text = "Analyze";
+            this.buttonStart.UseVisualStyleBackColor = true;
+            this.buttonStart.Click += new System.EventHandler(this.buttonStart_Click);
+            // 
+            // buttonChooseFile
+            // 
+            this.buttonChooseFile.Location = new System.Drawing.Point(30, 25);
+            this.buttonChooseFile.Name = "buttonChooseFile";
+            this.buttonChooseFile.Size = new System.Drawing.Size(189, 34);
+            this.buttonChooseFile.TabIndex = 0;
+            this.buttonChooseFile.Text = "Choose G-code file";
+            this.buttonChooseFile.UseVisualStyleBackColor = true;
+            this.buttonChooseFile.Click += new System.EventHandler(this.buttonChooseFile_Click);
+            // 
+            // labelFilePath
+            // 
+            this.labelFilePath.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.labelFilePath.Location = new System.Drawing.Point(225, 30);
+            this.labelFilePath.Name = "labelFilePath";
+            this.labelFilePath.Size = new System.Drawing.Size(507, 52);
+            this.labelFilePath.TabIndex = 2;
+            this.labelFilePath.Text = "File not selected.";
+            // 
+            // textBoxOriginX
+            // 
+            this.textBoxOriginX.Location = new System.Drawing.Point(148, 99);
+            this.textBoxOriginX.Name = "textBoxOriginX";
+            this.textBoxOriginX.Size = new System.Drawing.Size(108, 31);
+            this.textBoxOriginX.TabIndex = 1;
+            this.textBoxOriginX.Text = "0";
+            this.textBoxOriginX.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // textBoxOriginY
+            // 
+            this.textBoxOriginY.Location = new System.Drawing.Point(148, 136);
+            this.textBoxOriginY.Name = "textBoxOriginY";
+            this.textBoxOriginY.Size = new System.Drawing.Size(108, 31);
+            this.textBoxOriginY.TabIndex = 3;
+            this.textBoxOriginY.Text = "0";
+            this.textBoxOriginY.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // textBoxOriginZ
+            // 
+            this.textBoxOriginZ.Location = new System.Drawing.Point(148, 173);
+            this.textBoxOriginZ.Name = "textBoxOriginZ";
+            this.textBoxOriginZ.Size = new System.Drawing.Size(108, 31);
+            this.textBoxOriginZ.TabIndex = 4;
+            this.textBoxOriginZ.Text = "0";
+            this.textBoxOriginZ.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(30, 102);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(104, 25);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Reference X";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(30, 139);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(103, 25);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Reference Y";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(30, 176);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(103, 25);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Reference Z";
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.progressBar1.Location = new System.Drawing.Point(163, 236);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(569, 34);
+            this.progressBar1.TabIndex = 7;
+            // 
+            // textBoxResult
+            // 
+            this.textBoxResult.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBoxResult.Location = new System.Drawing.Point(30, 306);
+            this.textBoxResult.Multiline = true;
+            this.textBoxResult.Name = "textBoxResult";
+            this.textBoxResult.ReadOnly = true;
+            this.textBoxResult.ScrollBars = System.Windows.Forms.ScrollBars.Horizontal;
+            this.textBoxResult.Size = new System.Drawing.Size(702, 516);
+            this.textBoxResult.TabIndex = 8;
+            // 
+            // textBoxDensity
+            // 
+            this.textBoxDensity.Location = new System.Drawing.Point(516, 99);
+            this.textBoxDensity.Name = "textBoxDensity";
+            this.textBoxDensity.Size = new System.Drawing.Size(121, 31);
+            this.textBoxDensity.TabIndex = 9;
+            this.textBoxDensity.Text = "1.24";
+            this.textBoxDensity.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(355, 102);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(138, 25);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Density [g/cm3]";
+            // 
+            // textBoxDiameter
+            // 
+            this.textBoxDiameter.Location = new System.Drawing.Point(516, 136);
+            this.textBoxDiameter.Name = "textBoxDiameter";
+            this.textBoxDiameter.Size = new System.Drawing.Size(121, 31);
+            this.textBoxDiameter.TabIndex = 11;
+            this.textBoxDiameter.Text = "1.75";
+            this.textBoxDiameter.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(355, 139);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(131, 25);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "Diameter [mm]";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(763, 849);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBoxDiameter);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBoxDensity);
+            this.Controls.Add(this.textBoxResult);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxOriginZ);
+            this.Controls.Add(this.textBoxOriginY);
+            this.Controls.Add(this.textBoxOriginX);
+            this.Controls.Add(this.labelFilePath);
+            this.Controls.Add(this.buttonChooseFile);
+            this.Controls.Add(this.buttonStart);
+            this.Name = "Form1";
+            this.Text = "Moment of Inertia Estimator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Button buttonStart;
+        private Button buttonChooseFile;
+        private Label labelFilePath;
+        private TextBox textBoxOriginX;
+        private TextBox textBoxOriginY;
+        private TextBox textBoxOriginZ;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private ProgressBar progressBar1;
+        private TextBox textBoxResult;
+        private TextBox textBoxDensity;
+        private Label label4;
+        private TextBox textBoxDiameter;
+        private Label label5;
+    }
+}
---
+++ InertiaEstimator/Form1.cs
@@ -0,0 +1,352 @@
+using System.DirectoryServices;
+
+namespace InertialEstimator
+{
+    public partial class Form1 : Form
+    {
+        bool mbIsFileChoosed = false;
+        double mOriginX;
+        double mOriginY;
+        double mOriginZ;
+        double mDensity;
+        double mDiameter;
+
+        public Form1()
+        {
+            InitializeComponent();
+            this.HelpButton = true;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.HelpButtonClicked += Form1_HelpButtonClicked;
+        }
+
+        private void Form1_HelpButtonClicked(object? sender, System.ComponentModel.CancelEventArgs e)
+        {
+            MessageBox.Show("- It is a software for estimating the Moment of " +
+                "Inertia and Center of Mass of 3D printed parts.\r\n" +
+                "- It parses the G-code file created by PrusaSlicer.\r\n" +
+                "- The software is developed by Sunbin Kim. " +
+                "If you have any questions, please contact me at einsbon@gmail.com"
+            , "About this software.", MessageBoxButtons.OK);
+        }
+
+        private void buttonStart_Click(object sender, EventArgs e)
+        {
+            if (!mbIsFileChoosed)
+            {
+                MessageBox.Show("Please choose a file first.");
+                return;
+            }
+            if (!File.Exists(labelFilePath.Text))
+            {
+                MessageBox.Show("File not exist. " + labelFilePath.Text);
+                return;
+            }
+
+            if (!double.TryParse(textBoxOriginX.Text, out mOriginX)
+                || !double.TryParse(textBoxOriginY.Text, out mOriginY)
+                || !double.TryParse(textBoxOriginZ.Text, out mOriginZ)
+                || !double.TryParse(textBoxDensity.Text, out mDensity)
+                || !double.TryParse(textBoxDiameter.Text, out mDiameter)
+                || textBoxOriginX.Text == ""
+                || textBoxOriginY.Text == ""
+                || textBoxOriginZ.Text == ""
+                || textBoxDensity.Text == ""
+                || textBoxDiameter.Text == "")
+            {
+                MessageBox.Show("Enter numbers in the text boxes.");
+                return;
+            }
+
+
+            string[] lines = System.IO.File.ReadAllLines(labelFilePath.Text);
+            int lineLength = lines.Length;
+            int lineCount = 0;
+
+
+            double pastX = 0;
+            double pastY = 0;
+            double pastZ = 0;
+
+            double presentX = 0;
+            double presentY = 0;
+            double presentZ = 0;
+
+            bool bIsSupport = true;
+            bool bIsExtrude = false;
+
+            decimal wipeLength = 0;
+            decimal presentExtrude = 0;
+            decimal excludedExtrudeLength = 0;
+            decimal totalExtrudeLength = 0;
+
+            decimal ixx = 0;
+            decimal iyy = 0;
+            decimal izz = 0;
+
+            decimal extrudeSumX = 0;
+            decimal extrudeSumY = 0;
+            decimal extrudeSumZ = 0;
+
+            // gram of 1mm extruded filament
+            // (extruded * massFactor) is gram of filament extruded
+            double massFactor = (Math.PI * mDiameter * mDiameter * mDensity) / 4000.0;
+
+
+            progressBar1.Value = 0;
+            progressBar1.Minimum = 0;
+            progressBar1.Maximum = lineLength;
+            progressBar1.Step = 1;
+
+
+            foreach (string line in lines)
+            {
+                progressBar1.PerformStep();
+                lineCount++;
+
+                // Read comment to know the Freature Type of the print
+                if (line.Length > 6 && line.Substring(0, 6) == ";TYPE:")
+                {
+                    string type = line.Substring(6);
+                    switch (type)
+                    {
+                        case "Perimeter":
+                            bIsSupport = false;
+                            break;
+                        case "External perimeter":
+                            bIsSupport = false;
+                            break;
+                        case "Internal infill":
+                            bIsSupport = false;
+                            break;
+                        case "Solid infill":
+                            bIsSupport = false;
+                            break;
+                        case "Top solid infill":
+                            bIsSupport = false;
+                            break;
+                        case "Bridge infill":
+                            bIsSupport = false;
+                            break;
+                        case "Skirt/Brim":
+                            bIsSupport = true;
+                            break;
+                        case "Support material":
+                            bIsSupport = true;
+                            break;
+                        case "Support material interface":
+                            bIsSupport = true;
+                            break;
+                        case "Custom":
+                            bIsSupport = true;
+                            break;
+                        default:
+                            break;
+                    }
+                }
+                else if (line.Length > 2 && line.Substring(0, 3) == "G1 ")
+                {
+                    string[] parameters = line.Split(' ');
+
+                    if (parameters[1][0] == 'X' || parameters[1][0] == 'Y' || parameters[1][0] == 'Z')
+                    {
+                        pastX = presentX;
+                        pastY = presentY;
+                        pastZ = presentZ;
+                    }
+
+                    bIsExtrude = false;
+
+                    foreach (string parameter in parameters)
+                    {
+                        if (parameter[0] == 'X')
+                        {
+                            string xStr = parameter.Substring(1);
+                            presentX = Convert.ToDouble(xStr);
+                        }
+                        else if (parameter[0] == 'Y')
+                        {
+                            string yStr = parameter.Substring(1);
+                            presentY = Convert.ToDouble(yStr);
+                        }
+                        else if (parameter[0] == 'Z')
+                        {
+                            string zStr = parameter.Substring(1);
+                            presentZ = Convert.ToDouble(zStr);
+                        }
+                        else if (parameter[0] == 'F')
+                        {
+                            //string F = word.Substring(1);
+                            //textBoxF.Text = F[1];
+                        }
+                        else if (parameter[0] == 'E')
+                        {
+                            string extrudedStr = parameter.Substring(1);
+                            bIsExtrude = true;
+                            presentExtrude = Convert.ToDecimal(extrudedStr);
+                            totalExtrudeLength += presentExtrude;
+                        }
+                        else if (parameter[0] == ';')
+                        {
+                            break;
+                        }
+                    }
+
+                    if (!bIsExtrude)
+                    {
+                        continue;
+                    }
+
+                    if (presentExtrude < 0.0M)
+                    {
+                        wipeLength += presentExtrude;
+                        continue;
+                    }
+                    else
+                    {
+                        if (wipeLength < 0.0m)
+                        {
+                            // 아직 wipe가 남아있음
+
+                            if ((presentExtrude + wipeLength) > 0.001m)
+                            {
+                                // 이번 출력 명령으로 wipe가 모두 사라지는 경우. 0.001은 오차로 봄.
+                                presentExtrude = presentExtrude + wipeLength;
+                                wipeLength = 0.0m;
+                            }
+                            else
+                            {
+                                // 이번 출력 명령으로 wipe가 사라지지 않는 경우
+                                wipeLength += presentExtrude;
+                                continue;
+                            }
+                        }
+                        else if (wipeLength > 0.0m)
+                        {
+                            presentExtrude = presentExtrude + wipeLength;
+                            wipeLength = 0.0m;
+                        }
+                    }
+
+
+                    if (bIsSupport)
+                    {
+                        continue;
+                    }
+
+                    // 제거될 부분의 출력이 아닌 경우 아래 코드 수행.
+
+
+                    //
+                    // === 관성 모멘트 계산  ===
+
+                    // 1. 직선 동작 출력을 가는 막대라고 가정하고 관성모멘트 구하기
+
+                    // -- 각 축 기준으로 막대의 길이 
+                    double dX = presentX - pastX;
+                    double dY = presentY - pastY;
+                    double dZ = presentZ - pastZ;
+                    double lXSquare = dY * dY + dZ * dZ; // mm^2
+                    double lYSquare = dX * dX + dZ * dZ;
+                    double lZSquare = dX * dX + dY * dY;
+
+                    // -- 질량
+                    double mass = (double)presentExtrude * massFactor; // gram
+
+                    // -- 막대 중심에서의 관성 모멘트. I = (1/12) * m * (l^2)
+                    double icmX = (1.0 / 12.0) * mass * lXSquare; // g * mm^2
+                    double icmY = (1.0 / 12.0) * mass * lYSquare;
+                    double icmZ = (1.0 / 12.0) * mass * lZSquare;
+
+
+                    // 2. 누적 관성 모멘트 구하기
+
+                    // -- 기준점을 기준으로 한 막대의 중심 좌표
+                    double centerX = ((presentX + pastX) / 2) - mOriginX; // mm
+                    double centerY = ((presentY + pastY) / 2) - mOriginY;
+                    double centerZ = ((presentZ + pastZ) / 2) - mOriginZ;
+
+                    // -- 기준점의 각 축에서 막대의 중심까지의 거리
+                    double distFromXAxisToCenterSquare = centerY * centerY + centerZ * centerZ; // mm
+                    double distFromYAxisToCenterSquare = centerX * centerX + centerZ * centerZ;
+                    double distFromZAxisToCenterSquare = centerX * centerX + centerY * centerY;
+
+                    // - 평행축 정리. I = Icm + m * (d^2) 
+                    ixx += (decimal)(icmX + mass * distFromXAxisToCenterSquare); // g * mm^2
+                    iyy += (decimal)(icmY + mass * distFromYAxisToCenterSquare);
+                    izz += (decimal)(icmZ + mass * distFromZAxisToCenterSquare);
+
+
+                    //
+                    // === 무게 중심 구하기 위한 누적  ===
+                    extrudeSumX += (presentExtrude * (decimal)centerX); // 질량 아님. 무게 중심 구하기 위한 (출력 길이 * 출력 위치) 누적.
+                    extrudeSumY += (presentExtrude * (decimal)centerY);
+                    extrudeSumZ += (presentExtrude * (decimal)centerZ);
+
+
+                    //
+                    // === 총 질량과 무게중심을 구하기 위한 누적 (서포터 부분 제외) ===
+                    excludedExtrudeLength += presentExtrude;
+                }
+            }
+
+
+            // 누적 계산 완료 후 과정.
+
+
+            // 총 질량
+            double totalMass = (double)totalExtrudeLength * massFactor; // gram
+            double excludedMass = (double)excludedExtrudeLength * massFactor; // gram
+
+            // 무게중심
+            double centerOfMassX = (double)(extrudeSumX / excludedExtrudeLength);
+            double centerOfMassY = (double)(extrudeSumY / excludedExtrudeLength);
+            double centerOfMassZ = (double)(extrudeSumZ / excludedExtrudeLength);
+
+            // 무게중심에서의 관성모멘트를 구하기 위한 계산
+            // I = Icm + m * d^2
+            // Icm = I - m * d^2
+            double ixxCm = (double)ixx - excludedMass * (centerOfMassY * centerOfMassY + centerOfMassZ * centerOfMassZ);
+            double iyyCm = (double)iyy - excludedMass * (centerOfMassX * centerOfMassX + centerOfMassZ * centerOfMassZ);
+            double izzCm = (double)izz - excludedMass * (centerOfMassX * centerOfMassX + centerOfMassY * centerOfMassY);
+
+
+            textBoxResult.Text =
+                "Analyzed G-code file: " + labelFilePath.Text + "\r\n\r\n"
+                + String.Format("Total filament used [mm]: {0:0.000}\r\n", totalExtrudeLength)
+                + String.Format("Total filament used [g]: {0:0.000}\r\n\r\n", totalMass)
+                + String.Format("Filament used on the part to be removed [mm]: {0:0.000}\r\n", excludedExtrudeLength)
+                + String.Format("Mass excluding the part to be removed [g] {0:0.000}\r\n\r\n", excludedMass)
+                + String.Format("Moment of inertia at ({0}, {1}, {2}) [g / mm^2]:\r\n",
+                    mOriginX, mOriginY, mOriginZ)
+                + String.Format(" ixx: {0:0.000}\r\n", ixx)
+                + String.Format(" iyy: {0:0.000}\r\n", iyy)
+                + String.Format(" izz: {0:0.000}\r\n\r\n", izz)
+                + String.Format("Moment of inertia at Center Of Mass ({0:0.000}, {1:0.000}, {2:0.000}) [g / mm^2]:\r\n",
+                    centerOfMassX,
+                    centerOfMassY,
+                    centerOfMassZ)
+                + String.Format(" ixx: {0:0.000}\r\n", ixxCm)
+                + String.Format(" iyy: {0:0.000}\r\n", iyyCm)
+                + String.Format(" izz: {0:0.000}\r\n\r\n", izzCm)
+                ;
+            return;
+        }
+
+        private void buttonChooseFile_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog
+            {
+                Filter = "GCode files (*.gcode)|*.gcode|All files (*.*)|*.*",
+                FilterIndex = 1,
+                RestoreDirectory = true
+            };
+
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                labelFilePath.Text = openFileDialog.FileName;
+                mbIsFileChoosed = true;
+            }
+        }
+    }
+}
